e2378b9ab7cc6f53fcf3c665398daf505deb8f8f
KrushikBairaboina <krushikbairaboina07@gmail.com>
2026-01-01 20:57:09 +0530

    email reminder added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
-import toast, {Toaster} from 'react-hot-toast'
+import { useEffect } from "react";
+import toast, { Toaster } from "react-hot-toast";
 import { Route, Routes } from "react-router-dom";
 
 import ChatBox from "./pages/ChatBox";
@@ -10,16 +11,23 @@ import Layout from "./pages/Layout";
 import Login from "./pages/Login";
 import Meassages from "./pages/Meassages";
 import Profie from "./pages/Profie";
-import{useUser} from '@clerk/clerk-react'
+import { useUser, useAuth } from "@clerk/clerk-react";
 
 const App = () => {
-  const {user} = useUser()
+  const { user } = useUser();
+  const { getToken } = useAuth();
+
+  useEffect(() => {
+    if (user) {
+      getToken().then((token) => console.log(token));
+    }
+  }, [user, getToken]);
+
   return (
     <>
-    <Toaster/>
+      <Toaster />
       <Routes>
-        <Route path="/" element={ !user ? <Login /> : <Layout/>}>
-
+        <Route path="/" element={!user ? <Login /> : <Layout />}>
           <Route index element={<Feed />} />
           <Route path="messages" element={<Meassages />} />
           <Route path="messages/:userId" element={<ChatBox />} />
--- server/server.js
+++ server/server.js
@@ -3,14 +3,30 @@ import 'dotenv/config';
 import express from 'express';
 import {serve} from 'inngest/express'
 
+import { clerkMiddleware } from '@clerk/express'
 import connectDB from './configs/db.js';
 import {inngest, functions} from './inngest/index.js'
 
+// import messageRouter from './routes/messageRoutes.js';
+// import postRouter from './routes/postRoutes.js';
+// import storyRouter from './routes/storyRoutes.js';
+import userRouter from './routes/userRoutes.js';
+
 const app = express();
+
 await connectDB();
+
 app.use(express.json());
 app.use(cors());
-app.get('/', (req,res)=> res.send('server is running'))
+app.use(clerkMiddleware());
+
+app.get('/', (req, res)=> res.send('Server is running'))
 app.use('/api/inngest', serve({ client: inngest, functions }))
+app.use('/api/user', userRouter)
+// app.use('/api/post', postRouter)
+// app.use('/api/story', storyRouter)
+// app.use('/api/message', messageRouter)
+
 const PORT = process.env.PORT || 4000;
+
 app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`))
